9149132275b0fba3f5ce0b540d864f8e34c8e5a4
hato_yuji <nekoganeru@gmail.com>
2014-12-05 12:31:20 +0900

    Initial Commit

---
+++ SlideMenuControllerSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlideMenuControllerSwift
+//
+//  Created by 波戸 勇二 on 12/5/14.
+//  Copyright (c) 2014 Yuji Hato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
