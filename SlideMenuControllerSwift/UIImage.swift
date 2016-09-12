c099f2e013e794c67047c23f8ba306ecd320b56f
dekatotoro <nekoganeru@gmail.com>
2016-09-12 12:53:36 +0900

    Update to Swift2.3

--- SlideMenuControllerSwift/UIImage.swift
+++ SlideMenuControllerSwift/UIImage.swift
@@ -11,7 +11,7 @@ import UIKit
 extension UIImage {
     func trim(trimRect trimRect :CGRect) -> UIImage {
         if CGRectContainsRect(CGRect(origin: CGPointZero, size: self.size), trimRect) {
-            if let imageRef = CGImageCreateWithImageInRect(self.CGImage, trimRect) {
+            if let imageRef = CGImageCreateWithImageInRect(self.CGImage!, trimRect) {
                 return UIImage(CGImage: imageRef)
             }
         }
--- SlideMenuControllerSwift/UIView.swift
+++ SlideMenuControllerSwift/UIView.swift
@@ -11,7 +11,7 @@ import UIKit
 extension UIView {
     class func loadNib<T: UIView>(viewType: T.Type) -> T {
         let className = String.className(viewType)
-        return NSBundle(forClass: viewType).loadNibNamed(className, owner: nil, options: nil).first as! T
+        return NSBundle(forClass: viewType).loadNibNamed(className, owner: nil, options: nil)!.first as! T
     }
     
     class func loadNib() -> Self {
