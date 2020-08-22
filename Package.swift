6af7001a173bea5fc83d9f371d4c6a8927a47e69
Vladyslav Semenchenko <developer.semenchenko@gmail.com>
2020-08-22 17:42:46 +0300

    Test package.swift

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SlideMenuControllerSwift",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "SlideMenuControllerSwift",
+            targets: ["SlideMenuControllerSwift"]),
+    ],
+    dependencies: [
+
+    ],
+    targets: [
+        .target(
+            name: "SlideMenuControllerSwift",
+            dependencies: [],
+            path: "SlideMenuControllerSwift",
+            sources: ["SlideMenuControllerSwift"],
+            publicHeadersPath: "SlideMenuControllerSwift"
+        )
+    ]
+)
